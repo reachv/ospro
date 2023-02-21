508b140162c1b27fe8cf601b97e3ff7778cfeb89
froztty <91167567+froztty@users.noreply.github.com>
2023-02-21 20:19:19 +0100

    Post Friends Initialization, Friends Request Initial

--- app/src/main/java/com/example/osproject/Adapters/addFriendsAdapter.java
+++ app/src/main/java/com/example/osproject/Adapters/addFriendsAdapter.java
@@ -1,4 +1,70 @@
 package com.example.osproject.Adapters;
 
-public class addFriendsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.osproject.R;
+import com.parse.ParseUser;
+
+import java.util.Map;
+
+public class addFriendsAdapter extends RecyclerView.Adapter<addFriendsAdapter.Viewholder>{
+
+    //Onclick Override
+    public interface OnClickListener{
+        void onItemClicked(ParseUser user);
+    }
+
+    //Declaration
+    Map<String, ParseUser> userDisplay;
+    OnClickListener onClickListener;
+
+    //Constructor
+    public addFriendsAdapter(Map<String, ParseUser> userDisplay, OnClickListener onClickListener){
+        this.userDisplay = userDisplay;
+        this.onClickListener = onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+        ParseUser user = userDisplay.get(position);
+        holder.bind(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return userDisplay.size();
+    }
+
+    class Viewholder extends RecyclerView.ViewHolder{
+        //Declaration
+        TextView display;
+
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+            display = itemView.findViewById(android.R.id.text1);
+        }
+
+        public void bind(ParseUser user) {
+            display.setText(user.getUsername());
+            display.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickListener.onItemClicked(user);
+                }
+            });
+        }
+    }
 }
--- app/src/main/java/com/example/osproject/AddFriend.kt
+++ app/src/main/java/com/example/osproject/AddFriend.kt
@@ -2,12 +2,99 @@ package com.example.osproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.osproject.Adapters.addFriendsAdapter
+import com.example.osproject.GnS.Requests
+import com.parse.Parse
+import com.parse.ParseQuery
+import com.parse.ParseUser
+import org.w3c.dom.Text
+import kotlin.collections.Map as Map
 
 class AddFriend : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_friend)
 
+        //Declarations
+        var fullUser = HashMap<String, ParseUser>()
+        var users = HashMap<String, ParseUser>()
+        var input : EditText = findViewById(R.id.addfriendsET)
+        var rv : RecyclerView = findViewById(R.id.addFriendsRv)
+        var removeList = ArrayList<String>()
+        var adapter : addFriendsAdapter
+        var onClickListener : addFriendsAdapter.OnClickListener
 
+        //Creates Friend request
+        onClickListener = addFriendsAdapter.OnClickListener {
+            var request = Requests()
+            request.requester = ParseUser.getCurrentUser().objectId
+            request.requested = it.objectId
+            request.saveInBackground {
+                if(it != null){
+                    Log.e("AddFriends", "Create Request Exception: " + it)
+                    return@saveInBackground
+                }
+                Toast.makeText(this, "Successfully sent", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //RecyclerView bindings and adapter initialization
+        adapter = addFriendsAdapter(users, onClickListener)
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(this)
+
+        //Sends Query to backend for friends list
+        var parseQuery : ParseQuery<ParseUser> = ParseUser.getQuery()
+        parseQuery.findInBackground { objects, e ->
+            if(e != null){
+                Log.e("FriendsFragment", "Query Exception: " + e)
+                return@findInBackground
+            }
+            for(i in objects){
+                //Skips current user to not display
+                if(i.objectId == ParseUser.getCurrentUser().objectId)continue
+                //Full user list
+                fullUser.put(i.objectId, i)
+                //Adapter List
+                users.put(i.objectId, i)
+            }
+            adapter.notifyDataSetChanged()
+        }
+
+        //TextListener based on change
+        input.addTextChangedListener(object : TextWatcher{
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+            //Changes display list of users based on input of string
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.toString().isEmpty()){
+                    users = HashMap(fullUser)
+                    adapter.notifyDataSetChanged()
+                }
+                for(i in fullUser){
+                    //Iterates through users, matching string
+                    if(!i.value.username.contains(s.toString()))removeList.add(i.value.objectId.toString())
+                }
+                for(i in removeList){
+                    if(users.containsKey(i)){
+                        users.remove(i)
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
     }
 }
